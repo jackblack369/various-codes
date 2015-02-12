c4940df0df52d2b8402d1333ef5c92debcf09685
Christophe <christophe.egea@outlook.com>
2015-02-12 11:41:43 +0100

    Updates

--- variousdocs/CodePriceMinister/Price/Work/Codes/checkPrice.pas
+++
@@ -1,6 +0,0 @@
-#set($prixO=$tr.multiply($line.get(3),100,0))
-#if ($prixO<=100)
-1
-#else
-$tr.divide($prixO,100,2)
-#end
--- variousdocs/CodePriceMinister/Price/Work/Codes/cleanHTML.pas
+++
@@ -1,4 +0,0 @@
-#if($!line.get(5))
-#set($edito = $!line.get(5).replaceAll("</?[^>]*>"," "))
-#trunc($edito 490 '...')
-#end
